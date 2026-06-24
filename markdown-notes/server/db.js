d14a50ebbeda9899c467adf5e4e9e95856e85026
Ihsann <najihko1@gmail.com>
2026-06-24 17:28:15 +0700

    refactor(profile): remove markdown-notes project from profile repo

--- markdown-notes/server/db.js
+++
@@ -1,9 +0,0 @@
-import { JSONFilePreset } from 'lowdb/node';
-
-// Define the default structure for our database file
-const defaultData = { notes: [] };
-
-// Initialize db.json preset
-const db = await JSONFilePreset('db.json', defaultData);
-
-export default db;
